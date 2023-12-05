36547506d5b9d62ef496c49ae578ccfbaeb385b2
madanamaehanai <homehack@outlook.jp>
2023-12-05 10:28:42 +0900

    style修正

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -5,12 +5,14 @@ import { Link } from "react-router-dom";
 export default function Topbar() {
   return (
     <nav className="topbar">
+      <ul className="topbarlink">
       <Link to="/mysite/" className="topbarLink" style={{textDecoration: 'none'}}>
         <li className="home">ホーム</li>
       </Link>
       <Link to="/mysite/companyinformation" style={{textDecoration: 'none'}}>
         <li className="companyinformation">会社情報</li>
       </Link>
+      </ul>
     </nav>
   );
 }
