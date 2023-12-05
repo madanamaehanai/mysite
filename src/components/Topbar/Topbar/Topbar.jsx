4cbf1ca73a3ec8aae0180749cdd6658d2a45d843
madanamaehanai <homehack@outlook.jp>
2023-12-05 21:16:46 +0900

    ハンバーガーメニュー実装テスト

--- src/components/Topbar/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar/Topbar.jsx
@@ -8,43 +8,44 @@ export default function Topbar() {
     const mobileMenu = new MobileMenu();
     mobileMenu.DOM.container = document.querySelector("#topbarcomponent");
     const toggleHandler = () => mobileMenu._toggle();
-    mobileMenu.DOM.container.addEventListener('click', toggleHandler);
+    mobileMenu.DOM.container.addEventListener("click", toggleHandler);
     return () => {
-        mobileMenu.DOM.container.removeEventListener('click', toggleHandler);
+      mobileMenu.DOM.container.removeEventListener("click", toggleHandler);
     };
-}, []);
+  }, []);
 
   return (
     <div className="topbar">
-
       <div id="topbarcomponent" className="menu-open">
-        <div className="topbarlogo">
-          <p>〇〇工務店</p>
-        </div>
-        <div className="topbarcentercomponent">
-          <div className="topbarCenterComponent">
-            <ul className="topbarlink">
-              <Link
-                to="/mysite/"
-                className="topbarLink"
-                style={{ textDecoration: "none" }}
-              >
-                <li className="home">ホーム</li>
-              </Link>
-              <Link
-                to="/mysite/companyinformation"
-                style={{ textDecoration: "none" }}
-              >
-                <li className="companyinformation">会社情報</li>
-              </Link>
-            </ul>
+        <div className="TopBarComponent">
+          <div className="topbarlogo">
+            <p>〇〇工務店</p>
           </div>
-        </div>
-        <div className="topbarrightcomponent">
-          <div className="humburgermenu__btn">
-            <div className="humburgerbar1"></div>
-            <div className="humburgerbar2"></div>
-            <div className="humburgerbar3"></div>
+          <div className="topbarcentercomponent">
+            <div className="topbarCenterComponent">
+              <ul className="topbarlink">
+                <Link
+                  to="/mysite/"
+                  className="topbarLink"
+                  style={{ textDecoration: "none" }}
+                >
+                  <li className="home">ホーム</li>
+                </Link>
+                <Link
+                  to="/mysite/companyinformation"
+                  style={{ textDecoration: "none" }}
+                >
+                  <li className="companyinformation">会社情報</li>
+                </Link>
+              </ul>
+            </div>
+          </div>
+          <div className="topbarrightcomponent">
+            <div className="humburgermenu__btn">
+              <div className="humburgerbar1"></div>
+              <div className="humburgerbar2"></div>
+              <div className="humburgerbar3"></div>
+            </div>
           </div>
         </div>
       </div>
@@ -57,7 +58,7 @@ export default function Topbar() {
               <span className="sub-title">bbbbb</span>
             </a>
           </li>
-        <li className="mobile-menu__item">
+          <li className="mobile-menu__item">
             <a href="#">
               <span className="main-title">aaaaa</span>
               <span className="sub-title">bbbbb</span>
